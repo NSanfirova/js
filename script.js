d87cbcdc74d5bc8efce59b78cc9f31e2ec0fd39d
Nadezhda Sanfirova <exo76@yandex.ru>
2023-05-20 15:34:24 +0300

    length

--- script.js
+++ script.js
@@ -53,3 +53,10 @@ const myStr = "My name is " +myName + "and I am well!";
 const someAdjective = "very cool!";
 let myStr = "Learning to code is ";
 myStr+= someAdjective;
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
