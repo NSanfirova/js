31e67e05299e40df9b78f15bde5ed0d6daac4780
Nadezhda Sanfirova <exo76@yandex.ru>
2023-05-27 22:00:03 +0300

    buttleship

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const remainder = 11 % 3;
+/*const remainder = 11 % 3;
 
 let a = 3;
 let b = 17;
@@ -59,4 +59,35 @@ let lastNameLength = 0;
 const lastName = "Lovelace";
 
 // Only change code below this line
-lastNameLength = lastName.length;
+lastNameLength = lastName.length;*/
+
+
+/*BUTTLESHIP*/
+ let location1 = 3;
+ let location2 = 4;
+ let location3 = 5;
+ let guess;
+ let hits = 0;
+ let guesses = 0;
+ let isSunk = false;
+
+ while (isSunk == false) {
+    guess = prompt ("Огонь! (Введите число от 0 до 6)");
+    if (guess < 0 || guess > 6) {
+        alert ("Читать умеешь? От 0 до 6!!!!!!!");
+    } else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert ("Попал!!!");
+            hits = hits +1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("Ты утопил мой корабль (((((")
+            }
+        } else {
+            alert("Промазал!!!!! )))))))");
+        }
+    }
+ }
+let stats = "Тебе потребовалось " +guesses + " выстрелов, чтобы утопить мой корабль!";
+alert(stats);
