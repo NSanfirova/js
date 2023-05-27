b06b5d740ebf04b57125168ecaafcf1af6ccef64
Nadezhda Sanfirova <exo76@yandex.ru>
2023-05-27 22:32:30 +0300

    Tasks

--- script.js
+++ script.js
@@ -68,6 +68,43 @@ const lastName = "Lovelace";
 // Only change code below this line
 firstLetterOfLastName = lastName[0]; // Change this line
 
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+console.log(thirdLetterOfLastName);
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+console.log(lastLetterOfLastName);
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+console.log(secondToLastLetterOfLastName);
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myAdjective + " " +myNoun+" "+myVerb+" "+myAdverb; // Change this line
+// Only change code above this line
+console.log(wordBlanks)
+
 
 
 
